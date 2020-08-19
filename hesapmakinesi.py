34eb94566ce1103ff3373a8beab52ef7333e1c76
Mustafa Mert Kısa <56308007+mustafamertkisa@users.noreply.github.com>
2020-08-19 22:15:51 +0300

    hesap makinesi
    Python ile ilk tanıştığım zamanlarda hazırlamış olduğum temel düzeyde hesap makinesi çalışmam.

---
+++ hesapmakinesi.py
@@ -0,0 +1,43 @@
+print("""
+*************************************
+Hesap Makinesi Programına Hoşgeldiniz
+
+İşlem Numaraları;
+
+1. Toplama işlemi
+
+2. Çıkarma işlemi
+
+3. Çarpma işlemi
+
+4. Bölme işlemi
+
+5. Programdan çıkış
+*************************************
+""")
+
+while True:
+    a = int(input("Birinci sayıyı giriniz:"))
+    b = int(input("İkinci sayıyı giriniz:"))
+
+    islem = input("İşlem numarasını giriniz:")
+
+    if islem == "1":
+        print("{} + {} = {}".format(a,b,a+b))
+
+    elif islem == "2":
+        print("{} - {} = {}".format(a,b,a-b))
+
+    elif islem == "3":
+        print("{} x {} = {}".format(a,b,a*b))
+
+    elif islem == "4":
+        print("{} / {} = {}".format(a,b,a/b))
+
+    elif islem == "5":
+        print("Program sonlandırılıyor...")
+        break
+
+    else:
+        print("Lütfen geçerli bir işlem giriniz!")
+        continue
