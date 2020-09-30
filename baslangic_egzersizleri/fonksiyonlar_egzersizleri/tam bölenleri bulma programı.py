d70f557fabe28c0701e1e9fb43e769e82cbfe145
Mustafa Mert Kısa <56308007+mustafamertkisa@users.noreply.github.com>
2020-09-30 16:19:00 +0300

    python odevleri ve egzersizleri
    Python dilini öğrenme sürecimdeki konuları pekiştirmek amacıyla hazırladığım egzersizler ve okul dersim için hazırladığım ödevler.

---
+++ baslangic_egzersizleri/fonksiyonlar_egzersizleri/tam bölenleri bulma programı.py
@@ -0,0 +1,18 @@
+def tambolenleribulma(sayı):
+    tam_bolenler = []
+
+    for i in range(2,sayı):
+        if(sayı %i == 0):
+            tam_bolenler.append(i)
+    return tam_bolenler
+
+while True:
+
+    sayı=input("Sayı:")
+
+    if (sayı == "ç"):
+        print("Program sonlandırılıyor...")
+        break
+    else:
+        sayı=int(sayı)
+        print("Tam bölenler:",tambolenleribulma(sayı))
